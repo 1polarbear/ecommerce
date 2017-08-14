6d8d3a1ca41127ae2af307e3288ffa908ca288f4
MarcelaJackowski <mj464@nyu.edu>
2017-08-14 16:47:54 -0400

    update menu whiterabbit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,22 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def products_boys
+  end
+
+  def products_girls
+  end
+
+  def toys
+  end
+
+  def books
+  end
+
+  def gallery
+  end
+
+  def blog
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
 
-  root "products#index"
+  root "static_pages#home"
+  get 'static_pages/home'
+  get 'static_pages/about'
+
+
+  get 'products/products_girls'
+  get 'products/products_boys'
+  get 'products/toys'
+  get 'products/books'
+  get 'products/boys_prod_1'
+  get 'products/gallery'
+  get 'products/blog'
+
+
+  # get '/about', to: 'static_pages#about'
+
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show, :home]
   resource :user, except: [:new, :create, :destroy]
 
   namespace :current_user do
